d9e7b7903066445fcf1cbade894a9286fe7c7e1f
dung.nguyen <nguyenvietdungcs52@gmail.com>
2021-12-19 22:38:33 +0700

    remove unused define

--- contracts/IConsensusRule.sol
+++ contracts/IConsensusRule.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-enum ConsensusResult { ACCEPT, REJECT, WAIT }
-
 interface IConsensusRule {
     function isExistCandidate(address candidate) external returns (bool);
     function isAccepted(address candidate) external returns (bool);
